5612876aaba2c4bda9c6bd1e7f54da9a4d842403
WIN-M6GFPSNK9FF\Administrator <450092375@qq.com>
2021-05-17 21:23:40 +0800

    fix code

--- NettyLearn/src/NETTY/Netty.java
+++ NettyLearn/src/NETTY/Netty.java
@@ -3,9 +3,7 @@ package NETTY;
 public class Netty {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		EventLoopGroup bossGroup=new NioEventLoopGroup(1);
-		EventLoopGroup workGroup=new NioEventLoopGroup(8);
+
 	}
 
 }
